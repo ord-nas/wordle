db5c7b3ed92272885fe5c2f6f613ebeb8bed3f62
Sandro Young <ord-nas@users.noreply.github.com>
2022-01-23 02:01:08 -0800

    Another cycle of MinExpectedGuesses improvement.
    We've roughly hit a fixed point it seems.
    
    Not much improvement either.
    
    MinExpectedGuesses
    Average guesses: 3.67112
    1 guesses: 0.0234412 %
    2 guesses: 1.92218 %
    3 guesses: 38.9358 %
    4 guesses: 49.9062 %
    5 guesses: 8.4857 %
    6 guesses: 0.703235 %
    7 guesses: 0.0234412 %
    
    And now there's exactly one word where we will lose the game!

--- wordle.cpp
+++ wordle.cpp
@@ -260,8 +260,16 @@ double ComputeExpectedGuesses(const ResponseDistribution& distribution) {
 	expected_guesses = 0.0;
       } else if (count == 1) {
 	expected_guesses = 1.0;
+      } else if (count == 2) {
+	expected_guesses = 1.5;
       } else {
-	expected_guesses = 1.65 + 0.291 * log(count);
+	// From logarithmic fit on data from `python3 analyze.py stats/max_entropy_counts.csv`
+	// expected_guesses = 1.65 + 0.291 * log(count);
+	// From logarithmic fit on data from `python3 analyze.py stats/min_expected_guess_counts_0.csv`
+	expected_guesses = 1.51 + 0.289 * log(count);
+	// Note: `python3 analyze.py stats/min_expected_guess_counts_1.csv`
+	// shows basically the same equation, so we have roughly hit a fixed
+	// point.
       }
       guess_sum += P * expected_guesses;
     }
